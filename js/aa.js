e2cd10d69a1b146d3335ae52294f9cce6d161517
dlwlals1289 <dlwlals1289@gmail.com>
2025-04-17 16:01:45 +0900

    25.04.17 - module

---
+++ js/aa.js
@@ -0,0 +1,4 @@
+const xx = 1.7;
+const PI = 3.14;
+
+export default {PI};
---
+++ js/index.js
@@ -0,0 +1,12 @@
+import assert from 'assert';
+import moment from 'moment';
+import xx from './aa.js';
+console.log("hello");
+const hl = "hello";
+assert.equal(hl, "hello");
+
+const m = moment();
+console.log(m.startOf('day').fromNow());
+console.log(xx);
+
+// export {PI};
